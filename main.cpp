ec256935a94df0b076c59444cfd614790b16c702
maatt4code <49960850+maatt4code@users.noreply.github.com>
2019-04-26 19:15:00 +0100

    Update main.cpp
    Added Vadiadic Template Example

--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
 /* 
- * File for testing example code
+ * File for testing example code 
  */
 
 #include "TemplateImplememnationInCpp.h"
 #include "NonCopyable.hpp"
+#include "VariadicTemplate.hpp"
 
 int main(int argc, char** argv)
 {
     NonCopyable::test();
+    
     TemplateClass<Struct1> t1{Struct1()};
     TemplateClass<Struct2> t2{Struct2()};
+    
+    {
+        // odd number of inputs
+        std::stringstream ss;
+        VariadicTemplate::append(ss, 1, "two", 'c', false, 5.5);
+        std::cout << ss.str() << std::endl;
+    }
+    {
+        // even numbers of inputs
+        std::stringstream ss;
+        VariadicTemplate::append(ss, 1, "two", 'c', false);
+        std::cout << ss.str() << std::endl;
+    }
+    {
+        // no inputs - Well, this case is just invalid
+        //std::stringstream ss;
+        //VariadicTemplate::append(ss);
+        //std::cout << ss.str() << std::endl;
+    }
     return 0;
 }
