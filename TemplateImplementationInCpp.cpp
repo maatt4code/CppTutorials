72885e37da1c8485fb48db00a066533135635b02
maatt4code <49960850+maatt4code@users.noreply.github.com>
2019-04-27 10:38:53 +0100

    Update TemplateImplementationInCpp.cpp
    Comment Added

--- TemplateImplementationInCpp.cpp
+++ TemplateImplementationInCpp.cpp
@@ -1,8 +1,20 @@
+/**
+ * @file TemplateImplementationInCpp.cpp
+ *
+ * @brief This and the associated *.h file demonstrates how to write
+ * template implementation in *.cpp file rather than *.h
+ *
+ * @author M
+ *
+ */
+
 #include "TemplateImplememnationInCpp.h"
 #include <iostream>
 
+// Constructor prints out the wisdom of the concrete class
 template<class T>
 TemplateClass<T>::TemplateClass(const T& t) {
+    // SFINAE
     std::cout << t.wisdom << std::endl;
 }
 
