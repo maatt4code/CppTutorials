890f35fb7a64f7c8f469c04b4c068d4dbfec5c35
maatt4code <49960850+maatt4code@users.noreply.github.com>
2019-04-27 10:21:30 +0100

    Update VariadicTemplate.hpp
    Comments added

--- VariadicTemplate.hpp
+++ VariadicTemplate.hpp
@@ -1,6 +1,18 @@
+/**
+ * @file VariadicTemplate.hpp
+ *
+ * @brief Demonstration of how to construct variadic template function.
+ * this example simply takes a stringstream and your arguements and just
+ * append all your arguements to it
+ *
+ * @author M
+ *
+ */
+
 #pragma once
 
 #include <sstream>
+
 
 namespace VariadicTemplate
 {
@@ -22,12 +34,17 @@ void append(std::stringstream& ss, const T1& first, const T2& second)
     ss << first << ", " << second;
 };
 
+    
+// let's do 2 at a time to make it more complicated
 template<typename T1, typename T2, typename... Args>
 void append(std::stringstream& ss, const T1& first, const T2& second, Args... args)
 {
-    // let's do 2 at a time to make it more complicated
     std::cout << __PRETTY_FUNCTION__ << "\n";
     ss << first << ", " << second << ", ";
+    // pass the remaining arguments down the chain.
+    // This function may be called recursively or
+    // one of the above terminal functions may be called
+    // depending on the number of arguments remaining.
     append(ss, args...);
 };
 }
