488fdc081bb1155925f3ec5d42332bd31b6ac5fa
maatt4code <49960850+maatt4code@users.noreply.github.com>
2019-04-27 10:37:05 +0100

    Update TemplateImplementationInCpp.h
    Comment added

--- TemplateImplementationInCpp.h
+++ TemplateImplementationInCpp.h
@@ -1,7 +1,18 @@
+/**
+ * @file TemplateImplementationInCpp
+ *
+ * @brief This and the associated *.cpp file demonstrates how to write
+ * template implementation in *cpp file rather than *.h
+ *
+ * @author M
+ *
+ */
+
 #pragma once
 
 #include <string>
 
+// Some dummy structs that we instantiate our template class with
 struct Struct1
 {
     const std::string wisdom{"Options increase happiness"};
@@ -12,6 +23,7 @@ struct Struct2
     const std::string wisdom{"Too many options decrease happiness"};
 };
 
+// Template class we use for this example
 template<class T>
 class TemplateClass
 {
