2a2f326b50c01a1f5e42d27f1acbf333b0c92a2d
M <49960850+maatt4code@users.noreply.github.com>
2020-11-18 08:18:59 +0000

    Update String.hpp

--- Utils/String.hpp
+++ Utils/String.hpp
@@ -9,6 +9,7 @@ vector<string> split(const string& str, const string& delims) {
     vector<string> ret;
     size_t prev = 0;
     size_t pos = str.find_first_of(delims, prev);
+    // loop we go
     while(pos != string::npos) {
         const auto& tmp = str.substr(prev, pos - prev);
         if(!tmp.empty())
@@ -16,5 +17,7 @@ vector<string> split(const string& str, const string& delims) {
         prev = pos + 1;
         pos = str.find_first_of(delims, prev);
     }
+    // don't forget the last one
+    ret.push_back(str.substr(prev));
     return ret;
 }
